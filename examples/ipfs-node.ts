341877fd6822ad095c2236460de652fd5cb0470d
Alex Storm <info@alexstorm.ru>
2024-03-05 23:02:05 +0530

    fix config

--- examples/ipfs-node.ts
+++ examples/ipfs-node.ts
@@ -10,9 +10,8 @@ import {
     VolumeClaim,
     HostPath,
     Volume,
-    StatefullSet
+    StatefullSet,ExternalService
 } from "@solenopsys/synthetic";
-
 
 const routing = "dhtclient"
 
@@ -21,6 +20,13 @@ import { Container } from "../src/model";
 const name = "ipfs-node"
 const version = "0.1.39"
 const description = "IPFS node"
+const swarmKey="0472d6ec079252b82ba680c6b0e811c529d2dfa99f92cb7d13b5ad560ae10d7c"
+
+const nodes: { [key: string]: string } = {
+    "alpha-ipfs-node-service": "12D3KooWL2A1zj6oqVSRtanyZGDwJaEApxcg1JDAaCRbLayQ3aFd",
+    "bravo-ipfs-node-service": "12D3KooWHQNAJ2iLFFxpvH9YwE4r9MVMrYoQ9YxiVGY5L3qTJi1w",
+    "charlie-ipfs-node-service": "12D3KooWCG8XDMrBD8dSpEhoUhVUxAoGh3akYPVXFzKYVJ22yzQK"
+}
 
 export const dp = new Deploy({ name, version, description })
 
@@ -29,27 +35,26 @@ const gateway: Port = new Port("gateway", 8080)
 const rpc: Port = new Port("rpc", 5001)
 const p2p: Port = new Port("p2p", 4001)
 
+//services
+dp.add(new Service(name).addPort(rpc).addPort(gateway))
+dp.add(new LoadBalancer(name).addPort(p2p))
+
+for (let key in nodes)
+   dp.add(new ExternalService(key).addPort(p2p))
 
 
 // config maps
 function genSript() {
-    const bootstrap: string[] = [
-        "/dns4/alpha-ipfs-node-service.default.svc.cluster.local/tcp/4001/p2p/12D3KooWL2A1zj6oqVSRtanyZGDwJaEApxcg1JDAaCRbLayQ3aFd",
-        "/dns4/bravo-ipfs-node-service.default.svc.cluster.local/tcp/4001/p2p/12D3KooWHQNAJ2iLFFxpvH9YwE4r9MVMrYoQ9YxiVGY5L3qTJi1w",
-        "/dns4/charlie-ipfs-node-service.default.svc.cluster.local/tcp/4001/p2p/12D3KooWCG8XDMrBD8dSpEhoUhVUxAoGh3akYPVXFzKYVJ22yzQK"
-    ]
-
     let script = `#!/bin/sh
     set -ex
     ipfs bootstrap rm all\n\n`;
 
-    for (let value of bootstrap)
-        script += `ipfs bootstrap add "${value}"\n\n`;
+    for (let key in nodes)
+        script += `ipfs bootstrap add "/dns4/${key}.default.svc.cluster.local/tcp/${p2p.port}/p2p/${nodes[key]}"\n\n`;
 
     script += `ipfs config Routing --json '{ "Type": "${routing}" }' `;
     return script
 }
-
 
 const bootstrapConf = new ConfigMap("ipfs-bootstrap")
 bootstrapConf.set("bootstrap.sh", genSript())
@@ -58,12 +63,8 @@ const swarmConf = new ConfigMap("swarm-key")
 swarmConf.set("swarm.key", `
 /key/swarm/psk/1.0.0/
 /base16/
-0472d6ec079252b82ba680c6b0e811c529d2dfa99f92cb7d13b5ad560ae10d7c
+${swarmKey}
 `)
-
-//services
-dp.add(new Service(name).addPort(rpc).addPort(gateway))
-dp.add(new LoadBalancer(name).addPort(p2p))
 
 // ingress
 dp.add(new Ingress())
